f8bf788c98d4d952414e9a6e75295958f9c0f942
Antonio Doslic <antonio.doslic@plavatvornica.com>
2019-03-05 12:40:12 +0100

    news-api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,274 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+//import 'bootstrap/dist/css/bootstrap.css';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faIgloo } from '@fortawesome/free-solid-svg-icons';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import MaterialIcon from 'material-icons-react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+// import Card from 'react-bootstrap/Card';
+// import Image from 'react-bootstrap/Image';
+// import Search from '../src/img/nature.jpg';
+
+import ArticleCard from './components/ArticleCard'
+
+library.add(faIgloo)
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      articles: [],
+      inputSearch: '',
+      country: '',
+      category: ''
+    };
+  }
+
+filterSearch = (inputSearch='', country='', category='') => {
+    if(inputSearch=='' && country=='' && category=='') country="us"
+    let url = "https://newsapi.org/v2/top-headlines?q="+inputSearch+"&country="+country+"&category="+category+"&apiKey=f100b3cb4ed147d192f5237f42e0970c";
+    console.log(url);
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            articles: result.articles
+          });
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  componentDidMount() {
+    //alert("radi");
+    //event.preventDefault();
+    /*fetch("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f100b3cb4ed147d192f5237f42e0970c")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            articles: result.articles
+          });
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+      //fetch("https://newsapi.org/v2/top-headlines?q="+this.state.inputSearch+"+&country="+this.state.country+"&category="+this.state.category+"&apiKey=f100b3cb4ed147d192f5237f42e0970c")
+      */
+     this.filterSearch(this.state.inputSearch, this.state.country, this.state.category);
+  }
+  
+  handleClick = () => {
+    this.filterSearch(this.state.inputSearch, this.state.country, this.state.category);
+  }
+
+  removeInputs = () =>{
+    this.setState({
+      inputSearch: '',
+      country: '',
+      category: ''
+    });
+  }
+
+  updateInputValue = (event) =>{
+    //event.preventDefault();
+    this.setState({
+      inputSearch: event.target.value
+    });
+    //console.log(this.state.inputSearch);
+  }
+
+  changeCountry = (event) =>{
+    //event.preventDefault();
+    this.setState({
+      country: event.target.value
+    });
+    //console.log(event);
+    //console.log(event.target.value);
+  }
+  changeCategory = (event) =>{
+    //event.preventDefault();
+    this.setState({
+      category: event.target.value
+    });
+    //console.log(event);
+    //console.log(event.target.value);
+  }
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col sm={12}>
+              <InputGroup>
+                <FormControl
+                  onChange={event => this.updateInputValue(event)}
+                  placeholder="Search"
+                  className="inputField"
+                  value={this.state.inputSearch}
+                />
+
+                <DropdownButton
+                  //onChange = {event => this.changeCountry(event)}
+                  //onSelect={event => this.changeCountry(event)}
+                  as={InputGroup.Append}
+                  variant="outline-secondary"
+                  title = {(this.state.country=='')? "select country": this.state.country}
+                  id="input-group-dropdown"
+                  value={this.state.country}
+                  //onChange={this.handleChange}
+                >
+                  <Dropdown.Item 
+                    as="button"
+                    //onSelect={event => this.changeCountry(event)}
+                    onClick = {event => this.changeCountry(event)}
+                    value="us"
+                  >
+                    us
+                  </Dropdown.Item>
+
+                  <Dropdown.Item 
+                    as="button"
+                    //onSelect={event => this.changeCountry(event)}
+                    onClick = {event => this.changeCountry(event)}
+                    value="ru"
+                  >
+                    ru
+                  </Dropdown.Item>
+                  <Dropdown.Item 
+                    as="button"
+                    //onSelect={event => this.changeCountry(event)}
+                    onClick = {event => this.changeCountry(event)}
+                    value="de"
+                  >
+                    de
+                  </Dropdown.Item>
+                  <Dropdown.Item 
+                    as="button"
+                    //onSelect={event => this.changeCountry(event)}
+                    onClick = {event => this.changeCountry(event)}
+                    value="fr"
+                  >
+                    fr
+                  </Dropdown.Item>
+                  <Dropdown.Item 
+                    as="button"
+                    //onSelect={event => this.changeCountry(event)}
+                    onClick = {event => this.changeCountry(event)}
+                    value="gb"
+                  >
+                    gb
+                  </Dropdown.Item>
+                </DropdownButton>
+
+                <DropdownButton
+                  as={InputGroup.Append}
+                  variant="outline-secondary"
+                  title = {(this.state.category=='')? "select category": this.state.category}
+                  id="input-group-dropdown"
+                  value={this.state.category}
+                >
+                  <Dropdown.Item 
+                    as="button"
+                    onClick = {event => this.changeCategory(event)}
+                    value="general"
+                  >
+                    general
+                  </Dropdown.Item>
+                  <Dropdown.Item 
+                    as="button"
+                    onClick = {event => this.changeCategory(event)}
+                    value="business"
+                  >
+                    business
+                  </Dropdown.Item>
+                  <Dropdown.Item 
+                    as="button"
+                    onClick = {event => this.changeCategory(event)}
+                    value="entertainment"
+                  >
+                    entertainment
+                  </Dropdown.Item>
+                  <Dropdown.Item 
+                    as="button"
+                    onClick = {event => this.changeCategory(event)}
+                    value="sports"
+                  >
+                    sports
+                  </Dropdown.Item>
+                  <Dropdown.Item 
+                    as="button"
+                    onClick = {event => this.changeCategory(event)}
+                    value="health"
+                  >
+                    health
+                  </Dropdown.Item>
+                </DropdownButton>
+
+                <Button 
+                  onClick={this.handleClick}
+                  className="search"
+                  variant="outline-secondary"
+                >
+                  <MaterialIcon 
+                    icon="search"
+                  />
+                </Button>
+                <Button 
+                  onClick={this.removeInputs}
+                  className="borderRadius"
+                  variant="outline-secondary">
+                  <MaterialIcon 
+                    icon="close"
+                  />
+                </Button>
+              </InputGroup>
+            </Col>
+          </Row>
+          {
+            this.state.articles.slice(0, 5).map(article => (
+              <ArticleCard 
+                article={article}
+                key={article.title}
+              />
+            ))
+          }
+          {/* <ArticleCard article={this.state.articles && this.state.articles.length > 0 && this.state.articles[0]}/> */}
+        </Container>
       </div>
     );
+    
   }
 }
 
---
+++ src/components/ArticleCard/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import {Row, Col, Button, Card} from 'react-bootstrap'
+import slika from '../../img/imgNotFound.png'
+
+class ArticleCard extends Component {
+  constructor(props) {
+    super(props);
+    //console.log(props);
+  }
+  readArticle = () =>{
+      const url = this.props.article.url;
+      window.open(url, '_self');
+  }
+  render() {
+    return (
+      <div className="article ">
+        <Row className="noPadding">
+          <Card>
+            <Col sm={12}>
+              <Row>
+                <Col sm={5} >
+                  <div className="imgContainer" style={
+                    //{'backgroundImage': `url(${this.props.article.urlToImage})`}
+                    (this.props.article.urlToImage==null)?
+                      {'backgroundImage': `url(${slika}`}:
+                      {'backgroundImage': `url(${this.props.article.urlToImage})`}
+                    }>
+                  </div>
+                </Col>
+                <Col sm={7}>
+                {/*console.log(this.props)*/}
+                  <Card.Body>
+                    <Card.Title>{
+                      (this.props.article.title==null)?
+                      "Unknown title":
+                      this.props.article.title
+                    }</Card.Title>
+                    <Card.Subtitle>{
+                      (this.props.article.author==null)?
+                      'Author: unknown':
+                      'Author:'+this.props.article.author
+                      }</Card.Subtitle>
+                    <Card.Text>{
+                      (this.props.article.description==null)?
+                      "Description not found":
+                      this.props.article.description
+                      }</Card.Text>
+                    <Button 
+                      onClick={this.readArticle}
+                      variant="primary"
+                    >
+                    Read article
+                    </Button>
+                  </Card.Body>
+                
+                </Col>
+              </Row>
+              
+            </Col>
+          </Card>
+        </Row>
+      </div>
+      //return <h1>{this.props.greeting}</h1>;
+    )
+  }
+}
+  
+export default ArticleCard;
+  
